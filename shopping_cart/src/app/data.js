fd6a2e2cfe1a36802d462af44888dd7c9256f1e1
Franckalv <franciscozaalvarez@gmail.com>
2024-07-27 15:22:52 -0600

    Feat! Adding the products

--- shopping_cart/src/app/data.js
+++ shopping_cart/src/app/data.js
@@ -30,7 +30,8 @@ export const data=[
     urlImage:'https://www.zonadigitalsv.com/product/840272911228/image',
     quantity:1,
     description: 'El Razer Viper v3 Hyperspeed es un ratón inalámbrico para juegos que cuenta con la tecnología Razer HyperSpeed Wireless para una conexión inalámbrica ultrarrápida y fiable.'
-    }, {
+    }, 
+    {
         "id": 5,
         "title": "Teclado Mecánico Corsair K70 RGB MK.2",
         "price": 129,
@@ -77,5 +78,85 @@ export const data=[
         "urlImage": "https://www.zonadigitalsv.com/product/811659037541/image",
         "quantity": 1,
         "description": "El Razer BlackShark V2 es un headset gaming con drivers de 50 mm y sonido envolvente THX 7.1, ofreciendo una calidad de audio superior y comodidad para largas sesiones de juego."
+    },
+    {
+        "id": 11,
+        "title": "Monitor curvo HP Omen",
+        "price": 499,
+        "urlImage": "https://www.zonadigitalsv.com/product/197029618238/image",
+        "quantity": 1,
+        "description": "Para que todo fluya, esta pantalla cuenta con rápida velocidad de actualización de 165 Hz y tiempo de respuesta con Overdrive de 1 ms GTG que reduce el desenfoque por movimiento, 34 pulgadas de alta definición."
+    },
+    {
+        "id":12,
+        "title":"Monitor curvo ACER ED27O",
+        "price":299,
+        "urlImage":"https://www.zonadigitalsv.com/product/195133201094/image",
+        "quantity":1,
+        "description": "Pantalla: 68,6 cm (27) Full HD (1920 x 1080) 240 Hz, Curved 1500R Tecnología de Pantalla: Vertical Alignment (VA) (178°x178°) Adaptive Sync, Brillo: 250 cd/m²."
+    },
+    {
+        "id":13,
+        "title":"Mouse inalámbrico logitech M280",
+        "price":22.95,
+        "urlImage":"https://www.zonadigitalsv.com/product/097855109187/image",
+        "quantity":1,
+        "description": "M280 es la síntesis perfecta de funcionalidad, estética y confort. La forma asimétrica y la superficie estampada harán las delicias de tu mano y el botón rueda ancho mejorará tu experiencia de desplazamiento."
+    },
+    {
+        "id":14,
+        "title":"Mouse inalámbrico Genius NX-7015",
+        "price":12.55,
+        "urlImage":"https://www.zonadigitalsv.com/product/091163258667/image",
+        "quantity":1,
+        "description": "Con un diseño ergonómico que se adapta a tu mano, su conexión inalámbrica confiable te permite navegar con precisión y comodidad. Su sensor óptico de alta resolución de 1200 DPI garantiza una experiencia de uso duradera e eficiente."
+    },
+    {
+        "id":15,
+        "title":"Licencia de Microsoft Office 365",
+        "price":89,
+        "urlImage":"https://www.zonadigitalsv.com/product/020176326063/image",
+        "quantity":1,
+        "description": "Microsoft 365 Familia incluye hasta 6 TB de almacenamiento en la nube (1 TB por persona), características de seguridad avanzada y aplicaciones innovadoras para ti y tu familia, todo en un solo plan. La disponibilidad de las aplicaciones varía según el dispositivo o el idioma."
+    },
+    {
+        "id":16,
+        "title":"Licencia de Microsoft Windows 11 Pro",
+        "price":199.95,
+        "urlImage":"https://www.zonadigitalsv.com/product/889842906134/image",
+        "quantity":1,
+        "description": "Microsoft Windows 11 Pro te otorga el derecho legal a utilizar una versión completa y actualizada del sistema operativo Windows 11 Pro en un solo dispositivo. Esto significa que puedes instalar y ejecutar el sistema operativo en tu computadora personal o de trabajo, según los términos de la licencia."
+    },
+    {
+        "id":17,
+        "title":"Laptop HP Probook 440 Gen 8",
+        "price":199.95,
+        "urlImage":"https://www.zonadigitalsv.com/product/196188197721/image",
+        "quantity":1,
+        "description": "Procesador intel core i7 Gen 11, se trabaja con una memoria RAM DE 16 GB y de almacenamiento cuenta con un disco duro sólido de 512GB y cuenta con una pantalla de 14 pulgadas."
+    },
+    {
+        "id":18,
+        "title":"Laptop ASUS F1605ZA-WS74",
+        "price":199.95,
+        "urlImage":"https://www.zonadigitalsv.com/product/197105351615/image",
+        "quantity":1,
+        "description": "Procesador intel core i7 Gen 11, se trabaja con una memoria RAM DE 16 GB y de almacenamiento cuenta con un disco duro sólido de 512GB y cuenta con una pantalla de 14 pulgadas FHD 15.6P W11 WIFI+BT."
+    },
+    {
+        "id":19,
+        "title":"Mousepad logitech studio series",
+        "price":21,
+        "urlImage":"https://www.zonadigitalsv.com/product/097855171665/image",
+        "quantity":1,
+        "description": "Esta es la suave alfombrilla de mouse antideslizante que mejora tu espacio de trabajo. Hecha con materiales de alta calidad, la alfombrilla de mouse Logitech te ofrece la facilidad de deslizamiento y el confort que necesitas para tu mouse Logitech favorito."
+    },
+    {
+        "id":20,
+        "title":"Cable Xiaomi USB 2 en 1 micro USB y tipo C",
+        "price":7,
+        "urlImage":"https://www.zonadigitalsv.com/product/6970244524928/image",
+        "quantity":1,
+        "description": "Está diseñado para cargar y transferir datos de tus dispositivos. Su tamaño compacto te permite llevarlo en tu mochila ,Bolsa a cualquier lugar . Dentro del cable, varios cables trabajan juntos para proporcionar una carga segura y más rápida de hasta 2.4A."
     }
     ];
