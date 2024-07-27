483e597755dabe295bd7e79526af3b54086427c6
CruzM-224 <cruzmoreno022002@gmail.com>
2024-07-26 20:16:46 -0600

    Modales para las confirmaciones y descripcion

--- shopping_cart/src/app/components/Headers.jsx
+++ shopping_cart/src/app/components/Headers.jsx
@@ -1,87 +1,140 @@
-'use client'
-import { useState } from 'react';
-export const Headers = ({
-allProducts,
-setAllProducts,
-total,
-countProducts,
-setCountProducts,
-setTotal,
-}) => {
-const [active, setActive] = useState(false);
+'use client';
+import { useState, useEffect } from 'react';
+import ReactModal from 'react-modal';
 
-const onDeleteProduct = product => {
-const results = allProducts.filter(
-item => item.id !== product.id
-);
-setTotal(total - product.price * product.quantity);
-setCountProducts(countProducts - product.quantity);
-setAllProducts(results);
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+        zIndex: 1000
+    },
 };
-const onCleanCart = () => {
-setAllProducts([]);
-setTotal(0);
-setCountProducts(0);
-};
-return (
-<header>
-<h1>Tienda de Libros</h1>
-<div className='container-icon'>
-<div className='container-cart-icon'
-onClick={() => setActive(!active)}
->
-<img src="https://banner2.cleanpng.com/20180426/jxq/kisspng-computer-icons-shopping-cart-shopping-cart-decoration-5ae1d7b8251885.901689401524750264152.jpg"
-alt="carrito"
-className="icon-cart" />
-<div className='count-products'>
-<span id='contador-productos'>{countProducts}</span>
-</div>
-</div>
-<div
-className={`container-cart-products ${
-active ? '' : 'hidden-cart'
-}`}
->
-{allProducts.length ? (
-<>
-<div className='row-product'>
-{allProducts.map(product => (
-<div className='cart-product'
-key={product.id}>
-<div className='info-cart-product'>
-<span className='cantidad-producto-carrito'>
-{product.quantity}
-</span>
-<p className='titulo-producto-carrito'>
-{product.title}
-</p>
-<span className='precio-producto-carrito'>
-${product.price}
-</span>
-</div>
-<img
-src="https://static.vecteezy.com/system/resources/previews/018/887/462/original/signs-close-icon-png.png"
-alt="cerrar"
-className="icon-close"
-onClick={() => onDeleteProduct(product)}
-/>
-</div>
-))}
-</div>
-<div className='cart-total'>
-<h3>Total:</h3>
-<span className='total-pagar'>${total}</span>
-</div>
-<button className='btn-clear-all'
-onClick={onCleanCart}>
-Vaciar Carrito
-</button>
-</>
-) : (
-<p className='cart-empty'>El carrito está vacío</p>
-)}
-</div>
-</div>
-</header>
-);
+
+export const Headers = ({
+    allProducts,
+    setAllProducts,
+    total,
+    countProducts,
+    setCountProducts,
+    setTotal,
+}) => {
+    const [active, setActive] = useState(false);
+    const [modalIsOpen, setIsOpen] = useState(false);
+    const [productToDelete, setProductToDelete] = useState(null);
+    const [modalMessage, setModalMessage] = useState('');
+    const [modalTitle, setModalTitle] = useState('');
+    const [confirmAction, setConfirmAction] = useState(() => () => {});
+
+    useEffect(() => {
+        ReactModal.setAppElement('#cart-container');
+    }, []);
+
+    useEffect(() => {
+        if (productToDelete !== null) {
+            console.log('Product to delete has been set:', productToDelete);
+            openModal("¿Estás seguro de que quieres eliminar este artículo del carrito?", confirmDelete, "Confirmación de Eliminación");
+        }
+    }, [productToDelete]);
+
+    const openModal = (message, action, title) => {
+        setModalMessage(message);
+        setConfirmAction(() => action);
+        setModalTitle(title);
+        setIsOpen(true);
+    };
+
+    const closeModal = () => {
+        console.log('Closing modal ejecutandose');
+        setIsOpen(false);
+        setProductToDelete(null);
+    };
+
+    const confirmDelete = () => {
+        if (productToDelete) {
+            console.log('Product to delete:', productToDelete); // Debugging
+            const results = allProducts.filter(item => item.id !== productToDelete.id);
+            setTotal(total - productToDelete.price * productToDelete.quantity);
+            setCountProducts(countProducts - productToDelete.quantity);
+            setAllProducts(results);
+            closeModal();
+        } else {
+            console.log('Product to delete is still null');
+        }
+    };
+
+    const onDeleteProduct = (product) => {
+        setProductToDelete(product); // Set the product to delete
+        openModal("¿Estás seguro de que quieres eliminar este artículo del carrito?", confirmDelete, "Confirmación de Eliminación");
+    };
+
+    const onCleanCart = () => {
+        openModal("¿Estás seguro de que quieres vaciar el carrito?", () => {
+            setAllProducts([]);
+            setTotal(0);
+            setCountProducts(0);
+            closeModal();
+        }, "Confirmación de Vacío de Carrito");
+    };
+
+    return (
+        <header>
+            <h1>Tienda de tecnología, software y hardware.</h1>
+            <div className='container-icon'>
+                <div className='container-cart-icon' onClick={() => setActive(!active)}>
+                    <img src="/cart.svg" alt="carrito" className="icon-cart" />
+                    <div className='count-products'>
+                        <span id='contador-productos'>{countProducts}</span>
+                    </div>
+                </div>
+                <div id="cart-container" className={`container-cart-products ${active ? '' : 'hidden-cart'}`}>
+                    {allProducts.length ? (
+                        <>
+                            <div className='row-product'>
+                                {allProducts.map(product => (
+                                    <div className='cart-product' key={product.id}>
+                                        <div className='info-cart-product'>
+                                            <span className='cantidad-producto-carrito'>{product.quantity}</span>
+                                            <img src={product.urlImage} alt={product.title} className='imagen-producto-carrito' />
+                                            <p className='titulo-producto-carrito'>{product.title}</p>
+                                            <span className='precio-producto-carrito'>${product.price}</span>
+                                        </div>
+                                        <img
+                                            src="https://static.vecteezy.com/system/resources/previews/018/887/462/original/signs-close-icon-png.png"
+                                            alt="cerrar"
+                                            className="icon-close"
+                                            onClick={() => onDeleteProduct(product)}
+                                        />
+                                    </div>
+                                ))}
+                            </div>
+                            <div className='cart-total'>
+                                <h3>Total:</h3>
+                                <span className='total-pagar'>${total}</span>
+                            </div>
+                            <button className='btn-clear-all' onClick={onCleanCart}>
+                                Vaciar Carrito
+                            </button>
+                        </>
+                    ) : (
+                        <p className='cart-empty'>El carrito está vacío</p>
+                    )}
+                </div>
+                <ReactModal
+                    isOpen={modalIsOpen}
+                    onRequestClose={closeModal}
+                    style={customStyles}
+                    contentLabel={modalTitle}
+                >
+                    <h2>{modalTitle}</h2>
+                    <p>{modalMessage}</p>
+                    <button onClick={confirmAction} className="button-confirm">Sí</button>
+                    <button onClick={closeModal} className="button-cancel">No</button>
+                </ReactModal>
+            </div>
+        </header>
+    );
 };
--- shopping_cart/src/app/data.js
+++ shopping_cart/src/app/data.js
@@ -1,36 +1,81 @@
 export const data=[
     {
     id:1,
-    title:'Cien años de soledad',
-    price:100,
-    urlImage:'https://images.penguinrandomhouse.com/cover/9780525562443',
-    quantity:1
+    title:'Monitor Xiaomi G27I',
+    price:199,
+    urlImage:'https://www.zonadigitalsv.com/product/190997005871/image',
+    quantity:1,
+    description:'IPS rápido de 27 pulgadas. Esta pantalla de deportes electrónicos de 27 pulgadas tiene una resolución tan alta como 1920 × 1080. Adopta una pantalla LCD IPS rápida, lo que le permite responder rápidamente a eventos de tiempo rápido'
     },
     {
     id:2,
-    title:'El señor de los anillos(Trilogía)',
-    price:190,
-    urlImage:'https://media.vandalsports.com/i/1706x960/10-2023/20231031151343_1.jpg.webp',
-    quantity:1
+    title:'Monitor ACER V226HQL',
+    price:119,
+    urlImage: 'https://www.zonadigitalsv.com/product/195133157520/image',
+    quantity:1,
+    description: 'Monitor de 21.5 pulgadas con resolución Full HD de 1920 x 1080 píxeles. Tiene un tiempo de respuesta de 5 ms y un brillo de 250 cd/m². Conexiones VGA y DVI.'
     },
     {
     id:3,
-    title:'Cuentos de Barro',
-    price:30,
-    urlImage:'https://m.media-amazon.com/images/I/71mMRRgW9nL._SY425_.jpg',
-    quantity:1
+    title:'Mouse Logitech MX ERGO',
+    price:99.95,
+    urlImage:'https://www.zonadigitalsv.com/product/097855132666/image',
+    quantity:1,
+    description: 'El MX ERGO es un trackball ergonómico que se puede ajustar a la posición de la mano del usuario. Cuenta con una bisagra ajustable que permite al usuario elegir entre un ángulo de 0 y 20 grados.'
     },
     {
     id:4,
-    title:'Tierra de Infancia',
-    price:30,
-    urlImage:'https://marketsvstg.blob.core.windows.net/marketsv/0011705_tierra-de-infancia_510.jpeg',
-    quantity:1
+    title:'Mouse inalambrico Razer Viper V3 Hyperspeed',
+    price:89,
+    urlImage:'https://www.zonadigitalsv.com/product/840272911228/image',
+    quantity:1,
+    description: 'El Razer Viper v3 Hyperspeed es un ratón inalámbrico para juegos que cuenta con la tecnología Razer HyperSpeed Wireless para una conexión inalámbrica ultrarrápida y fiable.'
     }, {
-    id:5,
-    title:'Harry Potter Pack',
-    price:390,
-    urlImage:'https://images.ctfassets.net/usf1vwtuqyxm/1Q8VqFawdGifB5HNu3dEhj/9cf74381e52cc760a0ce21c6261588f2/Harry-Potter-2023-Paperbacks-Assets_White.jpg',
-    quantity:1
+        "id": 5,
+        "title": "Teclado Mecánico Corsair K70 RGB MK.2",
+        "price": 129,
+        "urlImage": "https://assets.corsair.com/image/upload/c_pad,q_auto,h_1024,w_1024,f_auto/products/Gaming-Keyboards/CH-9109010-ES/Gallery/K70_RGB_MK2_01.webp?width=1080&quality=85&auto=webp&format=pjpg",
+        "quantity": 1,
+        "description": "El Corsair K70 RGB MK.2 es un teclado mecánico de alto rendimiento con retroiluminación RGB dinámica y switches Cherry MX para una respuesta rápida y precisa."
+    },
+    {
+        "id": 6,
+        "title": "Teclado Gaming Logitech G Pro X",
+        "price": 99,
+        "urlImage": "https://resource.logitechg.com/w_1600,c_limit,q_auto,f_auto,dpr_1.0/d_transparent.gif/content/dam/gaming/en/products/pro-x-keyboard/pro-x-keyboard-gallery-1.png?v=1",
+        "quantity": 1,
+        "description": "El Logitech G Pro X es un teclado compacto y portátil con switches intercambiables, ideal para jugadores que buscan personalización y alto rendimiento."
+    },
+    {
+        "id": 7,
+        "title": "Webcam Logitech C920 HD Pro",
+        "price": 79,
+        "urlImage": "https://resource.logitech.com/w_1600,c_limit,q_auto,f_auto,dpr_1.0/d_transparent.gif/content/dam/logitech/en/products/webcams/c920/gallery/c920-gallery-1.png?v=1",
+        "quantity": 1,
+        "description": "La Logitech C920 HD Pro es una webcam con resolución Full HD 1080p y micrófonos estéreo integrados, ideal para videollamadas y transmisiones en vivo de alta calidad."
+    },
+    {
+        "id": 8,
+        "title": "Webcam Razer Kiyo",
+        "price": 89,
+        "urlImage": "https://i.pcmag.com/imagery/reviews/01qNhLsR5F6vh7Rl7W0p6Ae-1.fit_lim.size_1050x591.v1626272164.jpg",
+        "quantity": 1,
+        "description": "La Razer Kiyo es una webcam con iluminación anular ajustable y resolución Full HD 1080p, diseñada para streamers y creadores de contenido que necesitan una calidad de imagen excelente."
+    },
+    {
+        "id": 9,
+        "title": "Headset Gaming HyperX Cloud II",
+        "price": 109,
+        "urlImage": "https://techzone.com.sv/wp-content/uploads/2023/12/hyperx_cloud_ii_gunmetal_1_main_900x-1.webp",
+        "quantity": 1,
+        "description": "El HyperX Cloud II es un headset gaming con sonido envolvente virtual 7.1 y micrófono con cancelación de ruido, proporcionando una experiencia de juego inmersiva y comunicación clara."
+    },
+    {
+        "id": 10,
+        "title": "Headset Razer BlackShark V2",
+        "price": 129,
+        "urlImage": "https://www.zonadigitalsv.com/product/811659037541/image",
+        "quantity": 1,
+        "description": "El Razer BlackShark V2 es un headset gaming con drivers de 50 mm y sonido envolvente THX 7.1, ofreciendo una calidad de audio superior y comodidad para largas sesiones de juego."
     }
     ];
